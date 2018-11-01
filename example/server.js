d8b22d9368b4ba55c564a886f6ee214537d9aebd
Ilya Kutukov <ilya@getjerry.com>
2018-11-01 04:06:35 +0300

    * Initial commit

---
+++ example/server.js
@@ -0,0 +1,146 @@
+/* eslint-disable no-path-concat,prefer-template */
+/**
+ * @fileOverview GRPC-browser proxy server example
+ *
+ * based on:  grpc-web-node-server-example
+ *            https://github.com/grpc/grpc-web/tree/master/net/grpc/gateway/examples/echo/node-server
+ */
+
+const grpc = require('grpc');
+const logger = require('../src/logger')({ name: 'grpc-example-server' });
+
+const grpcServer = require('../src/grpcServer');
+const grpcProxy = require('../src/grpcProxy');
+
+/**
+ * @param {!Object} call
+ * @return {!Object} metadata
+ */
+const copyMetadata = (call) => {
+  const metadata = call.metadata.getMap();
+  const response_metadata = new grpc.Metadata();
+  for (let key in metadata) {
+    response_metadata.set(key, metadata[key]);
+  }
+  return response_metadata;
+};
+
+/**
+ * @param {!Object} call
+ * @param {function} callback
+ */
+const echo = (call, callback) => {
+  callback(null, {
+    message: call.request.message
+  }, copyMetadata(call));
+};
+
+/**
+ * @param {!Object} call
+ * @param {function} callback
+ */
+const echoAbort = (call, callback) => {
+  callback({
+    code: grpc.status.ABORTED,
+    message: 'Aborted from server side.'
+  });
+};
+
+/**
+ * @param {!{write: function, end: function}} call
+ */
+const serverStreamingEcho = (call) => {
+  const senders = [];
+  const sender = (message, interval) => {
+    return (callback) => {
+      call.write({
+        message: message
+      });
+      _.delay(callback, interval);
+    };
+  };
+  for (let i = 0; i < call.request.message_count; i++) {
+    senders[i] = sender(call.request.message, call.request.message_interval);
+  }
+  async.series(senders, () => {
+    call.end(copyMetadata(call));
+  });
+};
+
+const DEBUG = false;
+
+// noinspection JSUnusedGlobalSymbols
+/**
+ * GRPC upstream service conf
+ * @constant GRPC_UPSTREAM_CONF
+ */
+// noinspection JSUnusedGlobalSymbols
+const GRPC_UPSTREAM_CONF = {
+  service: 'EchoService',
+  serviceProtoPath: './example/echo.proto',
+  baseProtoPath: './example',  // Not mandatory
+  pkg: 'echo',
+  wsSchema: 'ws',
+  port: 8080,
+  host: 'localhost',
+  mountUrlPath: '/grpc-ws',
+  handlers: {
+    echo,
+    echoAbort,
+    serverStreamingEcho,
+  },
+};
+
+/**
+ * Some local web service conf
+ * @constant WEB_UPSTREAM_CONF
+ */
+const WEB_UPSTREAM_CONF = {
+  mountUrlPath: '/',
+  host: 'localhost',
+  port: 8000,
+  httpSchema: 'http',
+  wsSchema: 'ws',
+};
+
+
+/**
+ * Proxy endpoint conf
+ * @constant ENDPOINT_CONF
+ */
+const ENDPOINT_CONF = {
+  host: 'localhost',
+  port: '9001',
+  httpSchema: 'http',
+  wsSchema: 'ws',
+  tlsPort: null,
+  cert: null,
+  key: null,
+};
+
+/**
+ * Final conf
+ * @constant CONF
+ */
+const CONF = {
+  debug: DEBUG,
+  endpoint: ENDPOINT_CONF,
+  grpc: [GRPC_UPSTREAM_CONF],
+  web: [WEB_UPSTREAM_CONF],
+};
+
+
+/**
+ * Starts an RPC server that receives requests for the Greeter service at the
+ * sample server port
+ */
+const main = async () => {
+  const grpc = grpcServer(GRPC_UPSTREAM_CONF).listen();
+  await grpcProxy(CONF);
+};
+
+
+main()
+  .then(res => (res ? logger.info(res) : null))
+  .catch(e => logger.error(e));
+
---
+++ src/grpcProxy.js
@@ -0,0 +1,199 @@
+#!/usr/bin/env node
+/**
+ * Based in Danby proxy by `ericbets`
+ * https://github.com/ericbets/danby
+ */
+
+const fs = require('fs');
+const https = require('https');
+
+const express = require('express');
+const expressProxy = require('http-proxy-middleware');
+const ExpressWs = require('express-ws');
+const grpc = require('grpc');
+const helmet = require('helmet');
+const protobuf = require('protobufjs');
+const Logger = require('./logger');
+const logger = Logger({ name: 'grpcProxy' });
+
+const { promiseMap } = require('./utils');
+const { DEFAULT_ENDPOINT_CONF, DEFAULT_WEB_UPSTREAM_CONF, DEFAULT_GRPC_UPSTREAM_CONF } = require('./constants');
+
+// Global that will be called from eval executions
+const services = {};
+
+const getSvcRemote = (svcName) => `services['${svcName}'].remote.${svcName}`;
+
+const getSvcConnect = (svcName) => `services['${svcName}'].grpc`;
+
+const searchServices = (el = {}, services = {}) => {
+  if (el && el.nested) {
+    const f = el.nested;
+    for (const field in f) {
+      if (field !== 'parent') {
+        if (f.hasOwnProperty(field)) {
+          if (f[field] instanceof protobuf.Service) {
+            services[field] = f[field];
+          } else {
+            services = searchServices(f[field], services);
+          }
+        }
+      }
+    }
+  }
+  return services;
+};
+
+/**
+ * Run Proxy
+ * @param runConfig {{endpoint: Object, web: ?Array<Object>, grpc: Array<Object>, debug: ?boolean}}
+ * @return {Promise<*|Function>}
+ */
+const runProxy = async (runConfig = {}) => {
+  if (!runConfig) {
+    throw new Error('No configuration provided for proxy/runProxy method');
+  }
+  const endpointConf = Object.assign({}, DEFAULT_ENDPOINT_CONF, runConfig.endpoint || {});
+  const endpointPathStr = `${endpointConf.host}:${endpointConf.port}`;
+  const endpointGrpcPathStr = `${endpointConf.wsSchema}://${endpointPathStr}`;
+  const endpointsDescription = [
+    'GRPC Proxy running with following configuration:',
+    `- (${endpointConf.httpSchema}|${endpointConf.wsSchema})://${endpointPathStr}    ## Main endpoint`,
+  ];
+
+  /**
+   * GRPC
+   */
+  await promiseMap(
+    runConfig.grpc || [],
+    async (grpcUpstreamConf) => {
+      const conf = Object.assign({}, DEFAULT_GRPC_UPSTREAM_CONF, grpcUpstreamConf);
+      const grpcUpstreamStr = `${conf.wsSchema}://${conf.host}:${conf.port}`;
+      endpointsDescription.push([
+        `- ${grpcUpstreamStr}`,
+        '  ->  ',
+        `${endpointGrpcPathStr}${endpointConf.grpcMountUrlPath} - ${conf.pkg}.${conf.service}`,
+        `    ## GRPC proxy for ${conf.serviceProtoPath}`,
+      ].join(''));
+
+      const proto = await protobuf.loadSync(grpcUpstreamConf.serviceProtoPath);
+      const servicesDict = searchServices(proto);
+      const svc = servicesDict[grpcUpstreamConf.service];
+      // noinspection JSUnusedLocalSymbols
+      const methodNames = Object.keys(svc.methods);
+      let stub = null;
+      eval(`stub = proto.${grpcUpstreamConf.pkg}`);
+      services[grpcUpstreamConf.service] = {
+        cfg: conf,
+        methods: conf.methods,
+        remote: stub,
+        grpc: grpcUpstreamStr,
+      };
+    },
+  );
+
+  const httpServer = express();
+  ExpressWs(httpServer);
+
+  httpServer.use(helmet());
+
+  // WARNING: req param is used is
+  // noinspection JSUnusedLocalSymbols
+  const websocketHandler = (ws, req) => {
+    const debugOn = typeof (runConfig.debug) !== 'undefined' && runConfig.debug === true;
+    ws.on('message', (msg) => {
+
+      // WARNING! Methods below is used in Eval
+      // FIXME: Get rid of `eval` use, replace with dynamic loading
+      // eslint-disable-next-line no-unused-vars
+      // noinspection JSUnusedLocalSymbols
+      function respond(data) {
+        const str = JSON.stringify(data);
+        if (debugOn) {
+          logger.info(`S:${str}`);
+        }
+        ws.send(str);
+        ws.close();
+      }
+
+      // eslint-disable-next-line no-unused-vars
+      // noinspection JSUnusedLocalSymbols
+      function error(e) {
+        logger.error(e);
+      }
+
+      const obj = JSON.parse(msg);
+      const metadata = new grpc.Metadata();
+
+      if (services[obj.service].methods.includes(obj.method) && typeof (obj.payload === 'object')) {
+        if (typeof (obj.metadata) !== 'undefined') {
+          Object.keys(obj.metadata).forEach((name) => metadata.set(name, obj.metadata[name]));
+        }
+
+        const cmd = `client = new ${getSvcRemote(obj.service)}(${getSvcConnect(obj.service)}, grpc.credentials.createInsecure())`;
+
+        if (debugOn) {
+          logger.info(`C:${cmd}`);
+        }
+
+        eval(cmd);
+
+        logger.info('obj[\'method\']', obj.method, obj);
+        const call = `client.${obj.method}(${JSON.stringify(obj.payload)}, metadata , function (err, response) { if (err) { error(err); } else { respond(response); } });`;
+        if (debugOn) {
+          logger.info(`C:${call}`);
+        }
+        eval(call);
+      }
+    });
+  };
+  httpServer.ws(endpointConf.grpcMountUrlPath, websocketHandler);
+
+  /**
+   * Endpoint TLS conf
+   * TODO(ikutukov): verify that certs are working properly
+   *
+   * @type {*}
+   */
+
+  let tlsServer;
+  if (endpointConf.cert && endpointConf.key) {
+    const options = {
+      cert: fs.readFileSync(endpointConf.cert),
+      key: fs.readFileSync(endpointConf.key),
+    };
+    tlsServer = https.createServer(options, httpServer);
+  }
+  Object.assign(httpServer, endpointConf);
+
+  /**
+   * Web upstreams
+   */
+  runConfig.web.forEach((webUpstreamConf) => {
+    const conf = Object.assign({}, DEFAULT_WEB_UPSTREAM_CONF, webUpstreamConf);
+    const upstreamUrl = `${conf.host}:${conf.port}`;
+    endpointsDescription.push(`- (${conf.httpSchema}|${conf.wsSchema})://${upstreamUrl}  ->  (${endpointConf.httpSchema}|${endpointConf.wsSchema})://${endpointPathStr}${conf.filter ? '/[DYNAMIC RULES]' : conf.mountUrlPath}    ## Web proxy`);
+    if (webUpstreamConf.mountUrlPath) {
+
+      httpServer.use(conf.filter || conf.mountUrlPath, expressProxy({
+        ...conf,
+        target: `${conf.httpSchema}://${upstreamUrl}`,
+        changeOrigin: true,
+        ws: true,
+        logProvider: (conf) => Logger({ ...conf, name: 'http-proxy-middleware' }),
+      }));
+    }
+  });
+
+  /**
+   * Start
+   */
+  if (tlsServer) {
+    tlsServer.listen(endpointConf.tlsPort);
+  }
+  httpServer.listen(endpointConf.port);
+  endpointsDescription.forEach(ed => logger.info(ed));
+  return httpServer;
+};
+
+module.exports = runProxy;
